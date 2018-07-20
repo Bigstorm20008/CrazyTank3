a32d81b8fb622b0ac14daf3875a1fec12a91a953
Bigstorm <bigstorm@3g.ua>
2018-07-20 10:06:35 +0300

    namespace Gamestates

--- ConsoleView.h
+++ ConsoleView.h
@@ -32,6 +32,60 @@ private:
 
 };
 
+
+
+inline GraficsBuffer& ConsoleView::getBackBuffer()
+{
+	return backBuffer_;
+}
+
+
+inline void ConsoleView::prepeareBuffers(const unsigned int& width, const unsigned int& height)
+{
+	backBuffer_.intialize(width, height);
+	frontBuffer_ = backBuffer_;
+}
+
+
+inline void ConsoleView::setWindowSizesInSymbols(const unsigned int& width, const unsigned int& height)
+{
+	std::string systemCommand;
+	systemCommand.assign({ "mode con cols=" });
+	systemCommand.append(std::to_string(width));
+	systemCommand.append({ " lines=" });
+	systemCommand.append(std::to_string(height));
+
+	system(systemCommand.c_str());
+}
+
+
+inline void ConsoleView::moveWindowToScreenCenter()
+{
+	const int monitorWidth = GetSystemMetrics(SM_CXSCREEN);
+	const int monitorHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	const HWND consoleHwnd = GetConsoleWindow();
+	RECT consoleRect;
+	GetWindowRect(consoleHwnd, &consoleRect);
+	const unsigned int& consoleWidth = consoleRect.right - consoleRect.left;
+	const unsigned int& consoleHeight = consoleRect.bottom - consoleRect.top;
+
+	MoveWindow(consoleHwnd, (monitorWidth - consoleWidth) / 2,
+		(monitorHeight - consoleHeight) / 2,
+		consoleWidth, consoleHeight, true);
+}
+
+
+inline void ConsoleView::hideConsoleCursor()
+{
+	HANDLE consoleOuput = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO structCursorInfo;
+	GetConsoleCursorInfo(consoleOuput, &structCursorInfo);
+	structCursorInfo.bVisible = FALSE;
+	SetConsoleCursorInfo(consoleOuput, &structCursorInfo);
+}
+
+
 #endif // !CONSOLE_VIEW_H
 
 
